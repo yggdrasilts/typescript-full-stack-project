f7bd555d1f731bd8c439c569397d4bb217d81e57
David <david.lfelguera@nicepeopleatwork.com>
2019-11-22 17:28:59 +0100

    Update client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+/* Service */
+import { BugService } from "./shared/bug.service";
+
+/* Forms module */
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AddIssueComponent } from "./components/add-issue/add-issue.component";
+import { EditIssueComponent } from "./components/edit-issue/edit-issue.component";
+import { IssueListComponent } from "./components/issue-list/issue-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
+  providers: [BugService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
